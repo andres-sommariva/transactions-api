af0d13d7b29bf391d50e4f75d0c2697424ccddd4
Andrés Sommariva <andres.sommariva@gmail.com>
2026-03-02 18:47:20 -0300

    Add builder and hide 'id' field setter

--- transactions-data/src/main/java/com/mendel/transactions/model/TransactionRecord.java
+++ transactions-data/src/main/java/com/mendel/transactions/model/TransactionRecord.java
@@ -1,11 +1,17 @@
 package com.mendel.transactions.model;
 
 import java.time.Instant;
+
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Data;
+import lombok.Setter;
 
 @Data
+@Builder
 public class TransactionRecord {
 
+  @Setter(AccessLevel.NONE)
   private Long id;
   private Double amount;
   private String type;
